99954f98c9476e6901703f753b14047c8522593c
Jhonnatan ER <hytzy18@gmail.com>
2022-10-07 13:51:43 -0500

    avance2 IEEE754x32.py

--- paquetes/IEEE754x32.py
+++ paquetes/IEEE754x32.py
@@ -15,8 +15,16 @@ class IEEE754x32:
         b_fracionado = Fraccionario().get(abs(numero),23)
         binario = b_fracionado.split(',')
         # P3 Identificar el Exponente y el tipo de numero
-        # Se le suma 126 + exponentes y se empaqueta en 8 bits
-        exp_bin = Binario.get(len(binario[0]) + 126)
+        # Se identifica si tiene parte entera mayor o menor a 0
+        if float(binario[0]) > 0:
+            # Se le suma exponentes + 126
+            exp_dec = len(binario[0]) + 126
+            exp_bin = Binario.get(exp_dec)
+        else:
+            # Se le resta 128 - exponentes [VERIFICAR]
+            exp_dec = binario[1].index('1') + 1
+            exp_bin = Binario.get(128 - exp_dec)
+        # Se empaqueta en 8 bits
         exponente = '0'*(8-len(exp_bin)) + exp_bin
         # P4 Identificar la Mantisa
         # Normalizamos quitando el primer 1
