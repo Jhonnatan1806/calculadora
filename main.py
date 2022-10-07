2aeb95275cb9b8da1073c87cf523761fe0f1a319
Jhonnatan ER <hytzy18@gmail.com>
2022-10-07 15:49:11 -0500

    IEE754x32.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from paquetes.IEEE754x32 import IEEE754x32
+
 def main():    
     pass
+
+# s = '001110'
+# print(s.index('1')) #2
+# print(s.rindex('1')) #4
+
+ie = IEEE754x32().get(45)
+print(ie)
 
 if __name__ == '__main__':
     main()
--- paquetes/IEEE754x32.py
+++ paquetes/IEEE754x32.py
@@ -15,22 +15,30 @@ class IEEE754x32:
         b_fracionado = Fraccionario().get(abs(numero),23)
         binario = b_fracionado.split(',')
         # P3 Identificar el Exponente y el tipo de numero
-        # Se identifica si tiene parte entera mayor o menor a 0
-        if float(binario[0]) > 0:
+        # Se identifica si tiene parte entera o no
+        if int(binario[0]) > 0:
             # Se le suma exponentes + 126
             exp_dec = len(binario[0]) + 126
-            exp_bin = Binario.get(exp_dec)
+            exp_bin = Binario().get(exp_dec)
         else:
             # Se le resta 128 - exponentes [VERIFICAR]
             exp_dec = binario[1].index('1') + 1
-            exp_bin = Binario.get(128 - exp_dec)
+            exp_bin = Binario().get(128 - exp_dec)
         # Se empaqueta en 8 bits
         exponente = '0'*(8-len(exp_bin)) + exp_bin
         # P4 Identificar la Mantisa
-        # Normalizamos quitando el primer 1
-        
+        if int(binario[0]) > 0:
+            pos = binario[0].index('1')
+            mantisa = binario[0][pos+1:] + binario[1]
+        else:
+            mantisa = binario[1][exp_dec:]
+        bits = len(mantisa)
+        if bits<23:
+            mantisa = mantisa + '0'*(23-bits)
+        else:
+            mantisa = mantisa[:23]
         # P5 Unir las tres partes
-        return numero
+        return signo + exponente + mantisa
 
     def get(self,numero):
         return self.__algoritmo(numero)
