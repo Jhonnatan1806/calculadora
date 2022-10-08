72e7cce6007bfb50261f18d40ac2fc428cf0ef7b
Jhonnatan ER <hytzy18@gmail.com>
2022-10-08 16:21:47 -0500

    hexadecimal.py

--- paquetes/hexadecimal.py
+++ paquetes/hexadecimal.py
@@ -3,7 +3,10 @@ class Hexadecimal:
     def __init__(self):
         pass
     
+    # =======================================================
     # Algoritmo de base 10 a base 16
+    # =======================================================
+
     def dec_hex(self, numero):
         palabra = '' 
         while numero >= 1:
@@ -16,3 +19,22 @@ class Hexadecimal:
             else:
                 palabra = str(a) + palabra
         return palabra
+    
+    # =======================================================
+    # Algoritmo de base 16 a base 10
+    # =======================================================
+
+    def hex_dec(self,n_base16):
+        n_base10 = 0 
+        k = len(n_base16)
+        for i in range(k):
+            # numeros de 1 - 9
+            if ord(n_base16[i]) <= 57:
+                n = int(n_base16[i])
+                n_base10 += n*pow(16,k-1)
+            # numero de 10(A=65) - 15(F=70)
+            else:
+                n = int(ord(n_base16[i]))-55
+                n_base10 += n*pow(16,k-1)
+            k -=1 
+        return n_base10
---
+++ test/test_hexadecimal.py
@@ -0,0 +1,19 @@
+from paquetes.hexadecimal import Hexadecimal
+import unittest
+
+class Test_Hexadecimal(unittest.TestCase):
+
+    def setUp(self):
+        self.tmp = Hexadecimal()
+    
+    def tearDown(self):
+        del self.tmp
+
+    def test_dec_hex(self):
+        self.assertEquals('1A', self.tmp.dec_hex(26))
+    
+    def test_hex_dec(self):
+        self.assertEquals(26, self.tmp.hex_dec('1A'))
+
+if __name__ == '__main__':
+    unittest.main()
