c3d112985dcedbc6aef7e5c184c668fa92277069
Jhonnatan ER <hytzy18@gmail.com>
2022-10-07 19:27:31 -0500

    complemento1.py

--- paquetes/complemento1.py
+++ paquetes/complemento1.py
@@ -5,21 +5,33 @@ class Complemento1:
     def __init__(self):
         pass
 
-    # Algoritmo de base 10 a complemento 1
-    def dec_bin(self,numero):
+    # =======================================================
+    # Algoritmo de base 10 a base 2 complemento 1
+    # =======================================================
+    
+    def dec_bin(self, n_base10):
         # P1 Pasamos el numero a binario
-        numero = abs(numero)
-        binario = Binario().dec_bin(numero)
+        n_base10 = abs(n_base10)
+        n_base2 = Binario().dec_bin(n_base10)
         # P3 Invertimos el numero
-        binario = self.__invertir(binario)
-        return binario
+        n_base2 = self.__invertir(n_base2)
+        return n_base2
 
     # Funcion que invierte le numero binario
-    def __invertir(self, binario):
-        bin_invertido = ''
-        for i in range(len(binario)):
-            if binario[i]=='0':
-                bin_invertido += '1'
+    def __invertir(self, n_base2):
+        invertido = ''
+        for i in range(len(n_base2)):
+            if n_base2[i]=='0':
+                invertido += '1'
             else:
-                bin_invertido += '0'
-        return bin_invertido
+                invertido += '0'
+        return invertido
+
+    # =======================================================
+    # Algoritmo de base 2 complemento 1 a base 10
+    # =======================================================
+    
+    def bin_dec(self, n_base2):
+        n_base2 = self.__invertir(n_base2)
+        n_base10 = Binario().bin_dec(n_base2)
+        return n_base10
--- test/test_complento1.py
+++ test/test_complento1.py
@@ -4,10 +4,16 @@ import unittest
 class Test_Complemento1(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.tmp = Complemento1()
     
     def tearDown(self):
-        pass
+        del self.tmp
+
+    def test_dec_bin(self):
+        self.assertEquals('11101101', self.tmp.dec_bin(-18))
+    
+    def test_bin_dec(self):
+        self.assertEquals(18, self.tmp.bin_dec('11101101'))
 
 if __name__ == '__main__':
     unittest.main()
